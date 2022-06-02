cd39349ea232e716683932e19a462543cd2643a4
Gentrit Ibishi <44057937+GentritIbishi@users.noreply.github.com>
2022-06-02 14:32:35 +0200

    Checkpoint Library, 2:30
    2. Master sector about all app

--- app/src/main/java/fiek/unipr/mostwantedapp/fragment/ProfileFragment.java
+++ app/src/main/java/fiek/unipr/mostwantedapp/fragment/ProfileFragment.java
@@ -1,66 +1,278 @@
 package fiek.unipr.mostwantedapp.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import fiek.unipr.mostwantedapp.LoginActivity;
 import fiek.unipr.mostwantedapp.R;
+import fiek.unipr.mostwantedapp.helpers.CheckInternet;
+import fiek.unipr.mostwantedapp.helpers.CircleTransform;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View profile_fragment_view;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
+    private NavigationView nav_view;
+    private ImageView addReport;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public static final String PREFS_NAME = "LOG_PREF";
+    public static final String LOGIN_INFORMER_PREFS = "loginInformerPreferences";
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firebaseFirestore;
+    private DocumentReference documentReference;
+    private StorageReference storageReference;
+    private FirebaseStorage firebaseStorage;
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient gsc;
+    private TextView nav_header_name;
+    private ImageView verifiedBadge;
+    private CircleImageView nav_header_image_view;
+    private String user_anonymousID = null;
+    Integer balance;
+    String fullName, name, lastname, email, googleID, grade, parentName, address, phone, personal_number;
+    Uri photoURL;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        profile_fragment_view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        drawerLayout = profile_fragment_view.findViewById(R.id.drawerLayout);
+        toolbar = profile_fragment_view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        nav_view = profile_fragment_view.findViewById(R.id.nav_view);
+        nav_view.getMenu().getItem(0).setChecked(true);
+
+        //Get All Nav header to use elements like textview and any...
+        View headerView = nav_view.getHeaderView(0);
+        nav_header_name = headerView.findViewById(R.id.nav_header_name);
+        verifiedBadge = headerView.findViewById(R.id.verifiedBadge);
+        nav_header_image_view = headerView.findViewById(R.id.nav_header_image_view);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        user_anonymousID = getActivity().getIntent().getStringExtra("uid_anonymous");
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(getContext(), gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getContext());
+        if(account != null){
+            personal_number = null;
+            balance = 0;
+            name = account.getGivenName();
+            lastname = account.getFamilyName();
+            fullName = account.getDisplayName();
+            address = null;
+            email = account.getEmail();
+            parentName = null;
+            grade = "E";
+            photoURL = account.getPhotoUrl();
+            googleID = account.getId();
+
+            nav_header_name.setText(fullName);
+        }
+
+        if(firebaseAuth != null){
+            loadInformationFromFirebase(firebaseAuth);
+        }
+
+
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                Fragment fragment = null;
+                switch (id)
+                {
+                    case R.id.menu_group_home:
+                        fragment = new HomeFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.menu_group_search:
+                        fragment = new SearchFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.menu_group_profile:
+                        fragment = new ProfileFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.menu_group_logout:
+                        Logout(account);
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+
+        return profile_fragment_view;
     }
+
+    private void loadInformationFromFirebase(FirebaseAuth firebaseAuth) {
+        if(checkConnection()){
+            documentReference = firebaseFirestore.collection("users").document(firebaseAuth.getCurrentUser().getUid());
+            documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    String fullName = task.getResult().getString("fullName");
+                    String name = task.getResult().getString("name");
+                    String lastname = task.getResult().getString("lastname");
+                    String parentName = task.getResult().getString("parentName");
+                    String personal_number = task.getResult().getString("personal_number");
+                    String phone = task.getResult().getString("phone");
+                    String urlOfProfile = task.getResult().getString("urlOfProfile");
+                    String register_date_time = task.getResult().getString("register_date_time");
+                    String address = task.getResult().getString("address");
+//                    Integer balance = task.getResult().get
+                    String email = task.getResult().getString("email");
+                    String grade = task.getResult().getString("grade");
+
+                    setVerifiedBadge(firebaseAuth.getCurrentUser());
+
+                    storageReference = firebaseStorage.getReference().child("users/"+firebaseAuth.getCurrentUser().getUid()+"/profile_picture.jpg");
+                    if(storageReference != null) {
+                        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Picasso.get().load(uri).transform(new CircleTransform()).into(nav_header_image_view);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), R.string.image_not_set, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                    nav_header_name.setText(fullName);
+                }
+            });
+        }
+    }
+
+    private boolean checkConnection() {
+        CheckInternet checkInternet = new CheckInternet();
+        if(!checkInternet.isConnected(getContext())){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_home, fragment).commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        fragmentTransaction.addToBackStack(null);
+    }
+
+    private void Logout(GoogleSignInAccount account) {
+        //check for google login
+        if(account != null){
+            SignOut();
+        }
+
+        //check for phone authentication
+        if(firebaseAuth != null){
+            firebaseAuth.signOut();
+            sendUserToLogin();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getContext());
+        if (account != null) {
+            String fullName = account.getDisplayName();
+            String email = account.getEmail();
+            nav_header_name.setText(fullName);
+        }
+
+        if(firebaseUser == null){
+            sendUserToLogin();
+        }else {
+            String phone = firebaseUser.getPhoneNumber();
+            nav_header_name.setText(phone);
+            loadInformationFromFirebase(firebaseAuth);
+        }
+
+    }
+
+    public void sendUserToLogin() {
+        Intent loginIntent = new Intent(getActivity(), LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        getActivity().finish();
+    }
+
+    private void SignOut() {
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                getActivity().finish();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+            }
+        });
+    }
+
+    private void setVerifiedBadge(FirebaseUser firebaseUser) {
+        if(firebaseUser.isEmailVerified()){
+            verifiedBadge.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
