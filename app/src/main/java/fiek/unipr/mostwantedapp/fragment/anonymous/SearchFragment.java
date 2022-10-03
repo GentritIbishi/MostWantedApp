e03167e94de6c37d29612e0b32042d1f54b87247
Gentrit Ibishi <44057937+GentritIbishi@users.noreply.github.com>
2022-10-04 01:58:41 +0200

    Checkpoint, with out code logic!

--- app/src/main/java/fiek/unipr/mostwantedapp/fragment/anonymous/SearchFragment.java
+++ app/src/main/java/fiek/unipr/mostwantedapp/fragment/anonymous/SearchFragment.java
@@ -25,6 +25,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
@@ -46,18 +47,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fiek.unipr.mostwantedapp.R;
+import fiek.unipr.mostwantedapp.activity.GetStartedActivity;
 import fiek.unipr.mostwantedapp.activity.maps.user.MapsInformerActivity;
 import fiek.unipr.mostwantedapp.adapter.maps.MapsInformerPersonListAdapter;
 import fiek.unipr.mostwantedapp.models.LoginHistory;
 import fiek.unipr.mostwantedapp.models.Person;
 import fiek.unipr.mostwantedapp.utils.DateHelper;
+import fiek.unipr.mostwantedapp.utils.OnSwipeTouchListener;
 import fiek.unipr.mostwantedapp.utils.RecyclerViewInterface;
 import fiek.unipr.mostwantedapp.utils.StringHelper;
 
 public class SearchFragment extends Fragment implements RecyclerViewInterface {
 
     private View view;
+    private ScrollView scroll_anonymous;
     private TextView tv_anonymous_helper, tv_anonymous_hello;
+    private TextView intro, anonymous_dot1, anonymous_dot2;
     private TextInputEditText anonymous_search_filter;
     private RecyclerView anonymous_lvPersons;
     private LinearLayout search_anonymous_list_view1, search_anonymous_list_view2;
@@ -123,6 +128,19 @@ public class SearchFragment extends Fragment implements RecyclerViewInterface {
             }
         });
 
+        scroll_anonymous.setOnTouchListener(new OnSwipeTouchListener(getContext()){
+            public void onSwipeRight() {
+                intro.setText(R.string.get_started_description);
+                anonymous_dot1.setBackgroundResource(R.drawable.ic_dot_clicked);
+                anonymous_dot2.setBackgroundResource(R.drawable.ic_dot_unclicked);
+            }
+            public void onSwipeLeft() {
+                intro.setText(R.string.get_started_description2);
+                anonymous_dot2.setBackgroundResource(R.drawable.ic_dot_clicked);
+                anonymous_dot1.setBackgroundResource(R.drawable.ic_dot_unclicked);
+            }
+        });
+
         onBackPressed();
 
         return view;
@@ -159,8 +177,14 @@ public class SearchFragment extends Fragment implements RecyclerViewInterface {
 
     private void initializeFields() {
         anonymous_search_filter = view.findViewById(R.id.anonymous_search_filter);
+        scroll_anonymous = view.findViewById(R.id.scroll_anonymous);
         tv_anonymous_hello = view.findViewById(R.id.tv_anonymous_hello);
         tv_anonymous_helper = view.findViewById(R.id.tv_anonymous_helper);
+        anonymous_dot1 = view.findViewById(R.id.anonymous_dot1);
+        anonymous_dot2 = view.findViewById(R.id.anonymous_dot2);
+        intro = view.findViewById(R.id.intro);
+        search_anonymous_list_view1 = view.findViewById(R.id.search_anonymous_list_view1);
+        search_anonymous_list_view2 = view.findViewById(R.id.search_anonymous_list_view2);
         search_anonymous_list_switcher = view.findViewById(R.id.search_anonymous_list_switcher);
         anonymous_lvPersons = view.findViewById(R.id.anonymous_lvPersons);
         personArrayList = new ArrayList<>();
