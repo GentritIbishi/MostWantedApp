3fea5f947ce9f0905e19435fc11c1e3c79a7e949
Gentrit Ibishi <44057937+GentritIbishi@users.noreply.github.com>
2022-10-04 16:04:12 +0200

    Checkpoint settings in informer take information!

--- app/src/main/java/fiek/unipr/mostwantedapp/fragment/user/SettingsFragment.java
+++ app/src/main/java/fiek/unipr/mostwantedapp/fragment/user/SettingsFragment.java
@@ -1,8 +1,10 @@
 package fiek.unipr.mostwantedapp.fragment.user;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import fiek.unipr.mostwantedapp.R;
 
@@ -11,5 +13,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+        //we can use everywhere
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getContext() /* Activity context */);
+        String name = sharedPreferences.getString("emri i key", "vlera default");
+
     }
 }
